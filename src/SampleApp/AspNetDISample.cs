ca2038d06c4454b2c45bca77dec03fed8ebc2a1a
Damian Suess <djsuess@gmail.com>
2025-11-26 22:18:27 -0500

    ASP.NET and transporters samples in app

---
+++ src/SampleApp/AspNetDISample.cs
@@ -0,0 +1,45 @@
+// Copyright Xeno Innovations, Inc. 2025
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp;
+
+public class AspNetDISample
+{
+  /*
+  using System.Net;
+  using Lite.EventAggregator;
+  using Microsoft.AspNetCore.Builder;
+  using Microsoft.Extensions.DependencyInjection;
+
+  var builder = WebApplication.CreateBuilder(args);
+  builder.Services.AddEventAggregator();
+
+  // Pick ONE transport:
+  builder.Services.AddSingleton<IEventTransport>(sp =>
+    new NamedPipeTransport("server-requests-in","client-requests-in","server-replies-in"));
+
+  // or MemoryMappedTransport / TcpTransport (see earlier)
+  builder.Services.AddHostedService<EventAggregatorTransportHostedService>();
+
+  var app = builder.Build();
+
+  var agg = app.Services.GetRequiredService<IEventAggregator>();
+  agg.SubscribeRequest<Ping, Pong>(req => Task.FromResult(new Pong(req.Message + " from ASP.NET")));
+
+  app.MapGet("/request/{msg}", async (string msg) =>
+  {
+    var resp = await agg.RequestAsync<Ping, Pong>(new Ping(msg));
+    return resp.Message;
+  });
+
+  app.Run();
+
+  public record Ping(string Message);
+  public record Pong(string Message);
+
+   */
+}
---
+++ src/SampleApp/Transporters/NamedPipesDemo.cs
@@ -0,0 +1,36 @@
+// Copyright Xeno Innovations, Inc. 2025
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading.Tasks;
+using Lite.EventAggregator;
+using Lite.EventAggregator.Transporter;
+
+namespace SampleApp.Transporters;
+
+public class NamedPipesDemo
+{
+  public async Task RunDemoAsync()
+  {
+    Console.WriteLine("Named Pipes Demo");
+    var clientAgg = new EventAggregator();
+    var serverAgg = new EventAggregator();
+
+    var serverTransport = new NamedPipeEnvelopeTransport("server-requests-in", "client-requests-in", "server-replies-in");
+    var clientTransport = new NamedPipeEnvelopeTransport("client-requests-in", "server-requests-in", "client-replies-in");
+
+    await serverAgg.UseIpcEnvelopeTransportAsync(serverTransport);
+    await clientAgg.UseIpcEnvelopeTransportAsync(clientTransport);
+
+    serverAgg.Subscribe<Ping>(p => Console.WriteLine($"[Server] Publish: {p.Message}"));
+    serverAgg.SubscribeRequest<Ping, Pong>(req => Task.FromResult(new Pong(req.Message + " handled by server")));
+
+    await clientAgg.PublishEnvelopeAsync(new Ping("publish via pipes"));
+    var resp = await clientAgg.RequestAsync<Ping, Pong>(new Ping("request via pipes"));
+    Console.WriteLine($"[Client] Response: {resp.Message}");
+  }
+
+  public record Ping(string Message);
+
+  public record Pong(string Message);
+}
